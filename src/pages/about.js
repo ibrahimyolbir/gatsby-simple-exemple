86c313464a34b6f8445590f615d13ace5671e371
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2019-09-06 10:18:16 +0200

    gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+const SecondPage = () => (
+<>
+<Header headerText="Contact" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+</>
+)
+
+export default SecondPage
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+const IndexPage = () => (
+  
+  <div>
+    <Header headerText="Contact" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    </div>
+    </div>
+)
+
+export default IndexPage
